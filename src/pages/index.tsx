4e6fae68e86994d96f293dc96db97356130d884c
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-10-03 13:52:33 -0300

    criado o componente separador

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 import Banner from "../components/Banner";
 import Caracteristicas from "../components/Caracteristicas";
 import Header from "../components/Header";
+import Separador from "../components/Separador";
 
 export default function Home() {
   return (
@@ -9,6 +10,17 @@ export default function Home() {
       <Header />
       <Banner />
       <Caracteristicas />
+      <Separador />
+
+      <Heading
+        textAlign="center"
+        fontWeight="500"
+        mb={["5", "14"]}
+        fontSize={["lg", "3xl", "4xl"]}
+      >
+        Vamos nessa? <br />Então esolha seu continente
+      </Heading>
+
     </Flex>
   )
 }
